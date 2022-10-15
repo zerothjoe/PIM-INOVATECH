d0304540cc4432e41394304af82422af624f45d8
Joe Allysson <joeallysson67@gmail.com>
2022-10-15 20:27:51 -0300

    el muchacho de los ojos tristes

--- header.h
+++ header.h
@@ -1,10 +1,10 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
-#include "stdio.h"
-#include "string.h"
-#include "conio.h"
-#include "windows.h"
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <windows.h>
 #include <stdlib.h>
 
 
@@ -17,7 +17,7 @@
 
 
 // CORES
-
+#include <stdbool.h>
 #define RED2        "\x1b[31m"
 #define RESET       "\x1b[0m"
 #define RED         "\e[0;31m" //cores em ANSI utilizadas
@@ -36,3 +36,55 @@
 #define HRED        "\e[0;91m"
 
 #endif // HEADER_H_INCLUDED
+
+///// VARIAVEIS///////////////
+
+char adminlog[20] = "joe";
+char adminpass[20] = "lindo";
+char login[20];
+char senha[20];
+char ch = '\0';
+int ctecla=0;
+
+////////////////////////////
+
+
+///////////////////////////////////////////////////FUN��O LOG 2/////////////////////////////////////////////////////
+
+bool user_exists(char *user, char *password)
+{
+    FILE *file_ptr;
+
+    file_ptr = fopen("users.txt", "r");
+
+    if (!file_ptr)
+    {
+        printf("error: não foi possível abrir o arquivo de usuários.");
+        return 2;
+    }
+
+    // A linha do arquivo não pode ter mais do que 256 bytes
+    char current_line[256];
+
+    // Lê linha a linha do arquivo, e vai salvando a linha na variavel current_line
+    while (fgets(current_line, sizeof(current_line), file_ptr))
+    {
+        // separa a linha pelo caracter |
+        char *current_user = strtok(current_line, "|");
+        char *current_password = strtok(NULL, "|");
+
+        // remove o \n da senha
+        current_password[strcspn(current_password, "\n")] = '\0';
+
+        if (strcmp(user, current_user) == 0 && strcmp(password, current_password) == 0)
+        {
+            fclose(file_ptr);
+            return true;
+        }
+    }
+
+    fclose(file_ptr);
+    return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
--- main.c
+++ main.c
@@ -1,5 +1,99 @@
-int main()
+#include "header.h"
+
+int main() {
+memset(login, '\0', sizeof(login));
+memset(senha, '\0', sizeof(senha));
+printf("Digite o seu nome de usuario: ");
+scanf ("%s", login);
+fflush (stdin);
+printf("Digite a sua Senha: ");
+ while(ctecla<5)
+ {
+   ch = getch();
+
+   switch(ch)
+   {
+    case BACKSPACE:
+     if(ctecla>0)
+     {
+      fflush(stdin);
+      putch(BACKSPACE);
+      ch = '\0';
+      senha[ctecla] = ch;
+      ctecla--;
+      putch(SPACE);
+      putch(BACKSPACE);
+     }
+      break;
+    case TAB:
+     putch(TAB);
+     ctecla = 5;
+     break;
+    case ENTER:
+     putch(ENTER);
+     ctecla = 5;
+     break;
+    default:
+     senha[ctecla] = ch;
+     putch('*');
+     ctecla++;
+     break;
+   }
+ }
+if
+
+////////////////////////////////////////////PROGRAMA ADMIN COMEÇA AQUI////////////////////////////////////////////////////////
+
+(strcmp(adminlog, login) == 0 && strcmp(adminpass, senha) == 0){
+system("cls");
+printf(GREEN"\nLOGADO COM SUCESSO");
+printf(WHITE"\n Menu Inicial: \n 1 Cadastro de Clientes:  \n 2 Gerenciamento de Assinaturas: \n 3 Tela de Relatorios: \n 4 Cadastro de funcionarios");
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////PROGRAMA ADMIN TERMINA AQUI////////////////////////////////////////////////////////
+
+}
+
+
+
+else
+
+////////////////////////////////////////////PROGRAMA FUNC COMEÇA AQUI////////////////////////////////////////////////////////
+
+
 {
-    printf("Hello world!\n");
-    return 0;
+bool exists = user_exists(login, senha);
+
+if (exists == true)
+{
+system("cls");
+printf( GREEN"logado com sucesso\n");
+
+printf(WHITE"\n Menu Inicial: \n 1 Cadastro de Clientes:  \n 2 Gerenciamento de Assinaturas: \n 3 Tela de Relatorios:");
+
+
+
+
+
+
+
+
+////////////////////////////////////////////PROGRAMA FUNC TERMINA AQUI////////////////////////////////////////////////////////
+}
+
+
+else
+{
+system("cls");
+printf( RED"usuário ou senha inválidos\n");
+return 0;
+}
+}
 }
